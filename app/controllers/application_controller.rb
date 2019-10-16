9af49e1dbf8d73f80efc192c2061fc66217c0a67
jcgdxn <jcgdxn@hotmail.com>
2019-10-16 18:11:26 +0000

    L12_append_new-html-erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 
 
 class ApplicationController < ActionController::Base
+
+
+		def index
+		@quote = Quote.order("Random()").first
+		end
+
+
+		def new
+
+		@quote = Quote.new
+
+		end
+
 end
 
-def index
-	@quote = Quote.order("Random()").first
-end
+
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,6 +5,11 @@ class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").first
 	end	
+
+	def new
+
+
+	end
 end
 
 
